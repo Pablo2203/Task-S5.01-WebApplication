8de3f94c4e9971d658e291120b1d86c260f5e16e
Pablo Gómez San Joaquín <pablo.federico.gomez@gmail.com>
2025-09-08 20:11:47 +0200

    bugfix

--- src/main/java/cat/itacademy/s05/t02/n01/config/SecurityConfig.java
+++ src/main/java/cat/itacademy/s05/t02/n01/config/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig {
                                 "/v3/api-docs",
                                 "/v3/api-docs/",
                                 "/v3/api-docs.yaml",
-                                "/uploads/**"
+                                "/uploads/"
                         ).permitAll()
                         .pathMatchers("/api/appointments/requests").permitAll()
                         .pathMatchers("/api/admin/").hasRole("ADMIN")
