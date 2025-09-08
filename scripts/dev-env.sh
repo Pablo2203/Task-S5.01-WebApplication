9b2c8204c14427d9e4d67b7df59c6ea97a0f84a4
Pablo Gómez San Joaquín <pablo.federico.gomez@gmail.com>
2025-09-08 15:51:43 +0200

    bugfix

---
+++ scripts/dev-env.sh
@@ -0,0 +1,8 @@
+xport DB_USERNAME=app
+export DB_PASSWORD='TU_PASS'
+export JWT_SECRET='...clave...'
+export APP_CORS_ALLOWED_ORIGINS=http://localhost:5173
+export APP_UPLOADS_DIR="$PWD/uploads"
+export APP_UPLOADS_MAX_SIZE_BYTES=5242880
+scripts/dev-env.sh
+#!/usr/bin/env bash
