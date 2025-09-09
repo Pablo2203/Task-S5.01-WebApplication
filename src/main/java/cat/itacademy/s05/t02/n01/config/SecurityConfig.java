bc075c78c7bf321e5078bb66222ac9a10da7ce75
Pablo Gómez San Joaquín <pablo.federico.gomez@gmail.com>
2025-09-09 16:05:29 +0200

    admin

--- src/main/java/cat/itacademy/s05/t02/n01/config/SecurityConfig.java
+++ src/main/java/cat/itacademy/s05/t02/n01/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import cat.itacademy.s05.t02.n01.services.JwtService;
 import cat.itacademy.s05.t02.n01.services.UserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.ReactiveAuthenticationManager;
 import org.springframework.security.authentication.UserDetailsRepositoryReactiveAuthenticationManager;
 import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
@@ -12,14 +13,6 @@ import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.core.annotation.Order;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.web.server.util.matcher.ServerWebExchangeMatchers;
-import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.web.server.util.matcher.ServerWebExchangeMatchers;
-
-
 
 @Configuration
 @EnableReactiveMethodSecurity
@@ -27,51 +20,27 @@ public class SecurityConfig {
 
     @Bean
     @Order(0)
-    public SecurityWebFilterChain swaggerBasicChain(ServerHttpSecurity http,
-                                                    PasswordEncoder passwordEncoder) {
-// Usuario SÓLO para Swagger: swagger / Pablo2203
-        var uds = new MapReactiveUserDetailsService(
-                User.withUsername("swagger")
-                        .password(passwordEncoder.encode("Pablo2203"))
-                        .roles("SWAGGER")
-                        .build()
-        );
-        var basicAuthManager = new UserDetailsRepositoryReactiveAuthenticationManager(uds);
-        basicAuthManager.setPasswordEncoder(passwordEncoder);
-
-        return http
-                .securityMatcher(ServerWebExchangeMatchers.pathMatchers(
-                        "/swagger-ui.html",
-                        "/swagger-ui/**",
-                        "/webjars/**",
-                        "/v3/api-docs",
-                        "/v3/api-docs/**",
-                        "/v3/api-docs.yaml"
-                ))
-                .authenticationManager(basicAuthManager)      // usa el in-memory de arriba
-                .authorizeExchange(ex -> ex.anyExchange().authenticated())
-                .httpBasic(Customizer.withDefaults())
-                .csrf(ServerHttpSecurity.CsrfSpec::disable)
-                .build();
-    }
-
-    @Bean
-    @Order(1)
     public SecurityWebFilterChain securityWebFilterChain(
             ServerHttpSecurity http,
             JwtService jwtService,
             UserDetailsService userDetailsService,
             ReactiveAuthenticationManager authenticationManager) {
+
         return http
                 .securityContextRepository(new JwtSecurityContextRepository(jwtService, userDetailsService))
                 .authenticationManager(authenticationManager)
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .authorizeExchange(auth -> auth
+                        .pathMatchers(HttpMethod.OPTIONS).permitAll()
+                        .pathMatchers("/auth/**").permitAll()
+                        .pathMatchers("/swagger-ui.html", "/swagger-ui/**", "/webjars/**",
+                                "/v3/api-docs", "/v3/api-docs/**", "/v3/api-docs.yaml").permitAll()
                         .pathMatchers("/api/appointments/requests").permitAll()
-                        .pathMatchers("/api/admin/").hasRole("ADMIN")
-                        .pathMatchers("/api/professional/").hasAnyRole("PROFESSIONAL", "ADMIN")
+                        .pathMatchers("/api/admin/**").hasRole("ADMIN")
+                        .pathMatchers("/api/professional/**").hasAnyRole("PROFESSIONAL", "ADMIN")
                         .pathMatchers("/api/patient/**").hasAnyRole("PATIENT", "ADMIN")
-                        .anyExchange().authenticated())
+                        .anyExchange().authenticated()
+                )
                 .httpBasic(ServerHttpSecurity.HttpBasicSpec::disable)
                 .formLogin(ServerHttpSecurity.FormLoginSpec::disable)
                 .build();
